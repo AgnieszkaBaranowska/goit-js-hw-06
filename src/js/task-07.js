d322bbf04941002785a65bd737873fd0fa666ae8
AgnieszkaBaranowska <agn-bar503@wp.pl>
2022-11-20 13:14:16 +0100

    part of task-07

---
+++ src/js/task-07.js
@@ -0,0 +1,8 @@
+const slider = document.querySelector('#font-size-control');
+const textSize = document.querySelector('#text');
+
+const changeSize = () => {
+    
+}
+
+slider.addEventListener("input", changeSize);
