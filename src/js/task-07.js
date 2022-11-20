39ad85a7a7fca4cff6eb02517d22fda41166e947
AgnieszkaBaranowska <agn-bar503@wp.pl>
2022-11-20 15:23:56 +0100

    something that doesn't work - task-07

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -2,7 +2,12 @@ const slider = document.querySelector('#font-size-control');
 const textSize = document.querySelector('#text');
 
 const changeSize = () => {
-    
+    if (slider.value == textSize) {
+        textSize.text = "font-size-control";
+    }
+    else {
+        textSize.text = ".font-size-control + px"
+    }
 }
 
 slider.addEventListener("input", changeSize);
